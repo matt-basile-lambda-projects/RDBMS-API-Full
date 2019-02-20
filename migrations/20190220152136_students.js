61844173e3b2aee17caf1c13d5a6d1e7cf427d65
Matt Basile <matthewbasile2@gmail.com>
2019-02-20 15:28:32 -0500

    Built and configured migrations

---
+++ migrations/20190220152136_students.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('students', tbl=>{
+      tbl.increments();
+      tbl.string('name', 128).notNullable().unique();
+      tbl.timestamps(true, true);
+
+      tbl.integer('cohort_id').unsigned().references('id').inTable('cohorts')
+      .onDelete('CASCADE').onUpdate('CASCADE')
+  })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExist('students'); 
+};
