3bcafb611c8b042fdeb70e18586cafc5254bce4b
Matt Basile <matthewbasile2@gmail.com>
2019-02-20 16:05:33 -0500

    Getting Seeded data

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ const server = express();
 server.use(helmet())
 server.use(express.json());
 
+// GET
+server.get('/cohorts', async (req, res) =>{
+    try {
+     const cohorts = await db('cohorts');
+     res.status(200).json(cohorts)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// GET BY ID
+// Get A Cohorts Students
+// EDIT A COHORT
+// DELETE
+
+const port = process.env.PORT || 5000;
+server.listen(port, ()=> console.log(`\n Running on ${port}\n`))
